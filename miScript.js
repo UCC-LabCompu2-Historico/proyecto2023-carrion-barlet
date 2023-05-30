751b505671b4f354534a583e39ebcdae3dfa05a5
vanessa <vanessa.191010@gmail.com>
2023-05-30 15:51:36 -0300

    30/05

--- miScript.js
+++ miScript.js
@@ -13,6 +13,7 @@ function drawMap() {
 
 
     ctx.drawImage(img, 0, 100, 512, 300, 0,0, 768, 540);
+    ctx.beginPath();
 
     let origen = document.getElementById("origen").value;
 
@@ -20,55 +21,124 @@ function drawMap() {
         //buenos aires//
         ctx.fillStyle = "#69c2b8"
         ctx.fillRect(215, 400 , 10 , 10)
+        ctx.moveTo(215,400);
+
+        let destino = document.getElementById("destino").value;
+        if (destino==="Buenos Aires") {
+            //buenos aires//
+            //MESSAGE D ERREUR A METTRE
+
+        }
+        else if (destino==="Nueva York") {
+            //nueva york//
+            ctx.fillStyle = "#a778d9"
+            ctx.fillRect(150, 180 , 10 , 10)
+            ctx.lineTo(150,180);
+            ctx.strokeStyle="#ee4463";
+            ctx.lineWidth=3;
+            ctx.closePath();
+            ctx.stroke();
+            ctx.clearRect(10,10,100,100);
+        }
+
+        else if (destino==="Londres") {
+            //londres//
+            ctx.fillStyle = "#cb7ebc"
+            ctx.fillRect(320, 132 , 10 , 10)
+            ctx.lineTo(320,132);
+            ctx.strokeStyle="#eebe44";
+            ctx.lineWidth=3;
+            ctx.closePath();
+            ctx.stroke();
+            ctx.clearRect(10,10,100,100);
+
+        }
+        else if (destino==="Paris") {
+            //paris//
+            ctx.fillStyle = "#5f1988"
+            ctx.fillRect(330, 147 , 10 , 10)
+            ctx.lineTo(330,147);
+            ctx.strokeStyle="#69ee44";
+            ctx.lineWidth=3;
+            ctx.closePath();
+            ctx.stroke();
+            ctx.clearRect(100,100,100,100);
+
+
+        }
+
     }
     else if (origen==="Nueva York") {
         //nueva york//
         ctx.fillStyle = "#0520ce"
         ctx.fillRect(150, 180 , 10 , 10)
+        ctx.moveTo(150,180);
     }
 
     else if (origen==="Londres") {
         //londres//
         ctx.fillStyle = "#00abf1"
         ctx.fillRect(320, 132 , 10 , 10)
+        ctx.moveTo(320,132);
     }
     else if (origen==="Paris") {
         //paris//
         ctx.fillStyle = "#5867c4"
         ctx.fillRect(330, 147 , 10 , 10)
+        ctx.moveTo(330,147);
     }
 
 
-    let destino = document.getElementById("destino").value;
+    /*let destino = document.getElementById("destino").value;
 
     if (destino==="Buenos Aires") {
         //buenos aires//
         ctx.fillStyle = "#863063"
         ctx.fillRect(215, 400 , 10 , 10)
+        ctx.lineTo(215,400);
+        ctx.strokeStyle="#4488EE";
+        ctx.lineWidth=3;
+        ctx.closePath();
+        ctx.stroke();
+
     }
     else if (destino==="Nueva York") {
         //nueva york//
         ctx.fillStyle = "#a778d9"
         ctx.fillRect(150, 180 , 10 , 10)
+        ctx.lineTo(150,180);
+        ctx.strokeStyle="#4488EE";
+        ctx.lineWidth=3;
+        ctx.closePath();
+        ctx.stroke();
+
     }
 
     else if (destino==="Londres") {
         //londres//
         ctx.fillStyle = "#cb7ebc"
         ctx.fillRect(320, 132 , 10 , 10)
+        ctx.lineTo(320,132);
+        ctx.strokeStyle="#4488EE";
+        ctx.lineWidth=3;
+        ctx.closePath();
+        ctx.stroke();
+
     }
     else if (destino==="Paris") {
         //paris//
         ctx.fillStyle = "#5f1988"
         ctx.fillRect(330, 147 , 10 , 10)
-    }
+        ctx.lineTo(330,147);
+        ctx.strokeStyle="#4488EE";
+        ctx.lineWidth=3;
+        ctx.closePath();
+        ctx.stroke();
 
-    ctx.beginPath();
-    ctx.moveTo(330,147);
-    ctx.lineTo(150,180);
-    ctx.strokeStyle="#4488EE";
-    ctx.lineWidth=3;
-    ctx.stroke();
+    }
+    ctx.clearRect(10,10,100,100);*/
+
+
 
 
 }
