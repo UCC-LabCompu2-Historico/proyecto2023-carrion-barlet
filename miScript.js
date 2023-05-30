c1dc4012a6f750310984cd88b14d334c091ab9ea
vanessa <vanessa.191010@gmail.com>
2023-05-30 14:31:25 -0300

    orig-dest

--- miScript.js
+++ miScript.js
@@ -1,8 +1,16 @@
+/* Descripción de que hace la función
+* @method Nombre de la función
+* @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+* @param {number} ParámetroB - Explicación de que valor almacena ParámetroB
+* @return Valor que retorna
+*/
 function drawMap() {
+
     const canvas = document.getElementById("myCanvas");
     const ctx = canvas.getContext("2d");
     const img = new Image();
     img.src = "imagenes/continentes-de-la-tierra.png";
+
 
     ctx.drawImage(img, 0, 100, 512, 300, 0,0, 768, 540);
 
@@ -10,12 +18,12 @@ function drawMap() {
 
     if (origen==="Buenos Aires") {
         //buenos aires//
-        ctx.fillStyle = "#FFDE59"
+        ctx.fillStyle = "#69c2b8"
         ctx.fillRect(215, 400 , 10 , 10)
     }
-    else if (origen==="New York") {
-        //new york//
-        ctx.fillStyle = "#ce0505"
+    else if (origen==="Nueva York") {
+        //nueva york//
+        ctx.fillStyle = "#0520ce"
         ctx.fillRect(150, 180 , 10 , 10)
     }
 
@@ -25,6 +33,31 @@ function drawMap() {
         ctx.fillRect(320, 132 , 10 , 10)
     }
     else if (origen==="Paris") {
+        //paris//
+        ctx.fillStyle = "#5867c4"
+        ctx.fillRect(330, 147 , 10 , 10)
+    }
+
+
+    let destino = document.getElementById("destino").value;
+
+    if (destino==="Buenos Aires") {
+        //buenos aires//
+        ctx.fillStyle = "#863063"
+        ctx.fillRect(215, 400 , 10 , 10)
+    }
+    else if (destino==="Nueva York") {
+        //nueva york//
+        ctx.fillStyle = "#a778d9"
+        ctx.fillRect(150, 180 , 10 , 10)
+    }
+
+    else if (destino==="Londres") {
+        //londres//
+        ctx.fillStyle = "#cb7ebc"
+        ctx.fillRect(320, 132 , 10 , 10)
+    }
+    else if (destino==="Paris") {
         //paris//
         ctx.fillStyle = "#5f1988"
         ctx.fillRect(330, 147 , 10 , 10)
