a55237845c439e32f0eb54524b384ad70204d652
leaba <lea.barlet@ecam.fr>
2023-06-01 10:27:46 -0300

    all done

--- miScript.js
+++ miScript.js
@@ -18,10 +18,9 @@ function verifOrigen(origen){
         }
     }
     if (flag === 1) {
-        alert("origen ok");
     }
     else {
-        alert("AirFacu ne dessert pas cette origen");
+        alert("AirFacu no vuela a este origen");
         document.getElementById("origen").value=''
     }
 }
@@ -34,10 +33,9 @@ function verifDestino(destino){
         }
     }
     if (flug === 1) {
-        alert("destino ok");
     }
     else {
-        alert("AirFacu ne dessert pas cette destnio");
+        alert("AirFacu no vuela a este destino");
         document.getElementById("destino").value=''
     }
 }
@@ -51,175 +49,45 @@ function showMap() {
     ctx.beginPath();
 }*/
 
+function comprar(){
+    drawMap();
+    precio();
+}
 
 function drawMap() {
     const canvas = document.getElementById("myCanvas");
     const ctx = canvas.getContext("2d");
     const img = new Image();
     img.src = "imagenes/continentes-de-la-tierra.png";
-    ctx.drawImage(img, 0, 100, 512, 300, 0,0, 768, 540);
+    ctx.drawImage(img, 0, 100, 512, 300, 0, 0, 768, 540);
     ctx.beginPath();
     let origen = document.getElementById("origen").value;
-     for (let i = 0; i < ciudades.length; i++) {
+    for (let i = 0; i < ciudades.length; i++) {
         if (origen === ciudades[i]) {
             ctx.fillStyle = "#35b70a"
-            ctx.fillRect(coordX[i], coordY[i], 10 , 10)
-            ctx.moveTo(coordX[i],coordY[i]);
+            ctx.fillRect(coordX[i], coordY[i], 10, 10)
+            ctx.moveTo(coordX[i], coordY[i]);
         }
     }
     let destino = document.getElementById("destino").value;
     for (let i = 0; i < ciudades.length; i++) {
         if (destino === ciudades[i]) {
             ctx.fillStyle = "#ce0505"
-            ctx.fillRect(coordX[i], coordY[i], 10 , 10)
-            ctx.lineTo(coordX[i],coordY[i]);
-            ctx.strokeStyle="#1f3cad";
-            ctx.lineWidth=3;
+            ctx.fillRect(coordX[i], coordY[i], 10, 10)
+            ctx.lineTo(coordX[i], coordY[i]);
+            ctx.strokeStyle = "#1f3cad";
+            ctx.lineWidth = 3;
             ctx.closePath();
             ctx.stroke();
         }
     }
-
 }
 
-
-
-
-function blo() {
-
-    const canvas = document.getElementById("myCanvas");
-    const ctx = canvas.getContext("2d");
-    const img = new Image();
-    img.src = "imagenes/continentes-de-la-tierra.png";
-
-
-    ctx.drawImage(img, 0, 100, 512, 300, 0,0, 768, 540);
-    ctx.beginPath();
-
-    let origen = document.getElementById("origen").value;
-
-    if (origen==="Buenos Aires") {
-        //buenos aires//
-        ctx.fillStyle = "#69c2b8"
-        ctx.fillRect(215, 400 , 10 , 10)
-        ctx.moveTo(215,400);
-
-        let destino = document.getElementById("destino").value;
-        if (destino==="Buenos Aires") {
-            //buenos aires//
-            //MESSAGE D ERREUR A METTRE
-
-        }
-        else if (destino==="Nueva York") {
-            //nueva york//
-            ctx.fillStyle = "#a778d9"
-            ctx.fillRect(150, 180 , 10 , 10)
-            ctx.lineTo(150,180);
-            ctx.strokeStyle="#ee4463";
-            ctx.lineWidth=3;
-            ctx.closePath();
-            ctx.stroke();
-            ctx.clearRect(10,10,100,100);
-        }
-
-        else if (destino==="Londres") {
-            //londres//
-            ctx.fillStyle = "#cb7ebc"
-            ctx.fillRect(320, 132 , 10 , 10)
-            ctx.lineTo(320,132);
-            ctx.strokeStyle="#eebe44";
-            ctx.lineWidth=3;
-            ctx.closePath();
-            ctx.stroke();
-            ctx.clearRect(10,10,100,100);
-
-        }
-        else if (destino==="Paris") {
-            //paris//
-            ctx.fillStyle = "#5f1988"
-            ctx.fillRect(330, 147 , 10 , 10)
-            ctx.lineTo(330,147);
-            ctx.strokeStyle="#69ee44";
-            ctx.lineWidth=3;
-            ctx.closePath();
-            ctx.stroke();
-            ctx.clearRect(100,100,100,100);
-
-
-        }
-
-    }
-    else if (origen==="Nueva York") {
-        //nueva york//
-        ctx.fillStyle = "#0520ce"
-        ctx.fillRect(150, 180 , 10 , 10)
-        ctx.moveTo(150,180);
-    }
-
-    else if (origen==="Londres") {
-        //londres//
-        ctx.fillStyle = "#00abf1"
-        ctx.fillRect(320, 132 , 10 , 10)
-        ctx.moveTo(320,132);
-    }
-    else if (origen==="Paris") {
-        //paris//
-        ctx.fillStyle = "#5867c4"
-        ctx.fillRect(330, 147 , 10 , 10)
-        ctx.moveTo(330,147);
-    }
-
-
-    /*let destino = document.getElementById("destino").value;
-
-    if (destino==="Buenos Aires") {
-        //buenos aires//
-        ctx.fillStyle = "#863063"
-        ctx.fillRect(215, 400 , 10 , 10)
-        ctx.lineTo(215,400);
-        ctx.strokeStyle="#4488EE";
-        ctx.lineWidth=3;
-        ctx.closePath();
-        ctx.stroke();
-
-    }
-    else if (destino==="Nueva York") {
-        //nueva york//
-        ctx.fillStyle = "#a778d9"
-        ctx.fillRect(150, 180 , 10 , 10)
-        ctx.lineTo(150,180);
-        ctx.strokeStyle="#4488EE";
-        ctx.lineWidth=3;
-        ctx.closePath();
-        ctx.stroke();
-
-    }
-
-    else if (destino==="Londres") {
-        //londres//
-        ctx.fillStyle = "#cb7ebc"
-        ctx.fillRect(320, 132 , 10 , 10)
-        ctx.lineTo(320,132);
-        ctx.strokeStyle="#4488EE";
-        ctx.lineWidth=3;
-        ctx.closePath();
-        ctx.stroke();
-
-    }
-    else if (destino==="Paris") {
-        //paris//
-        ctx.fillStyle = "#5f1988"
-        ctx.fillRect(330, 147 , 10 , 10)
-        ctx.lineTo(330,147);
-        ctx.strokeStyle="#4488EE";
-        ctx.lineWidth=3;
-        ctx.closePath();
-        ctx.stroke();
-
-    }
-    ctx.clearRect(10,10,100,100);*/
-
-
-
-
+function precio(){
+    let personas = document.getElementById("nb_personas").value;
+    let equipaje = document.getElementById("nb_equipaje").value;
+    let precio_billete = 1000;
+    let precio_equipaje = equipaje * 100;
+    document.getElementById('precio_billete').innerText = (precio_billete * personas + precio_equipaje).toString();
 }
+
